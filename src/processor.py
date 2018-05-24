11464327db25cab64866942282ed115fbbc68c76
Brandon Krull <btkrull@gmail.com>
2018-05-23 21:39:14 -0700

    adding first Session pass + tests

--- src/processor.py
+++ src/processor.py
@@ -28,6 +28,41 @@ class User(object):
         return self.ip == other_user.ip
 
 
+class Session(object):
+    """
+    Session class that is attached to a User. A Session's attributes
+    are the necessary attrs for keeping track of a User's activity.
+    """
+
+    def __init__(self, ip, date, time, count=0, length=0, end=0):
+        self.ip = ip
+        self.length = length
+        self.count = count
+        self.end = end
+        try:
+            self.dt = parse(date + ' ' + time)
+        except ValueError:
+            raise ValueError('Invalid or unknown date/time string')
+
+    def __repr__(self):
+        return 'dt: {}, count: {}, length:{}'.format(self.dt, self.count,
+                                                     self.length)
+
+    def close(self, output_handle):
+        self.end = self.dt + relativedelta(seconds=self.length)
+        dt_open = self.format_dt_for_output(self.dt)
+        dt_close = self.format_dt_for_output(self.end)
+
+        output_line = '{},{},{},{},{}\n'.format(self.ip, dt_open, dt_close,
+                                                self.length, self.count)
+        print output_line
+        output_handle.write(output_line)
+
+
+    def _format_dt_for_output(self):
+        return ' '.join(self.dt.isoformat().split('T'))
+
+
 def get_value_indices(keys):
     """
     truth contains the column names that we actually need
@@ -49,3 +84,46 @@ def get_raw_log_data(fname):
 
     return data
 
+
+def process_logs(raw_data, inactivity_time, output_handle):
+    open_sessions = []
+    for record in raw_data:
+        user = User(record['ip'])
+        print user
+        now = parse(record['date'] + ' ' + record['time'])
+
+        if user not in open_sessions:
+            user.sess = Session(count=1, length=0, **record)
+            open_sessions.append(user)
+            #             print 'adding new user: {}'.format(user.ip)
+            #             print user.session
+        else:
+            sess = open_sessions[open_sessions.index(user)].sess
+            length = relativedelta(now, sess.dt).seconds - sess.length
+
+            print "Length of existing session: {}".format(length)
+            if length > inactivity_time:
+                sess.close(output_handle)
+                open_sessions.remove(user)
+            else:
+                sess.dt = now
+                sess.count += 1
+
+        for u in open_sessions:
+            if u == user: continue
+
+            length = relativedelta(now, u.sess.dt).seconds - u.sess.length
+
+            if length > inactivity_time:
+                u.sess.length = length
+                sess.close(output_handle)
+                open_sessions.remove(u)
+    """
+    Once the end of the log is reached, close all remaining open sessions
+    """
+    for u in open_sessions:
+        length = relativedelta(now, u.sess.dt).seconds - u.sess.length
+
+        u.sess.length = length
+        u.sess.close(output_handle)
+        open_sessions.remove(u)
---
+++ tests/test_session.py
@@ -0,0 +1,44 @@
+from src.processor import Session
+from dateutil.parser import parse
+
+
+record = {'ip': '123.123.123.1', 'date': '0366-12-24', 'time': '23:59:59'}
+baddate = {'ip': '123.123.123.1', 'date': '0x66-12-24', 'time': '23:59:59'}
+badtime = {'ip': '123.123.123.1', 'date': '0366-12-24', 'time': 'z3:59:59'}
+
+
+def test_session_init_bad_date():
+    try:
+        sess = Session(**baddate)
+    except ValueError as exc:
+        assert True
+    else:
+        assert False
+
+
+def test_session_init_bad_time():
+    try:
+        sess = Session(**badtime)
+    except ValueError as exc:
+        assert True
+    else:
+        assert False
+
+
+def test_session_init():
+    sess = Session(**record)
+
+    assert sess.ip == '123.123.123.1'
+    assert sess.dt == parse(record['date'] + ' ' + record['time'])
+    assert sess.length == 0
+    assert sess.count == 0
+    assert sess.end == 0
+
+
+def test_date_output_format():
+    expected = record['date'] + ' ' + record['time']
+
+    sess = Session(**record)
+    output = sess._format_dt_for_output()
+
+    assert output == expected
