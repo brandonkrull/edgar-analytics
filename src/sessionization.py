88479847574e3d02767aa1b18abdbc98fab87c54
Brandon Krull <btkrull@gmail.com>
2018-05-23 22:51:23 -0700

    adding outputfile removal since i'm using append while processing

--- src/sessionization.py
+++ src/sessionization.py
@@ -1,9 +1,9 @@
 from argparse import ArgumentParser
+from os import remove
 from processor import get_inactivity_value, get_raw_log_data, process_logs
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-
     parser.add_argument('log', help='Name of input log file in CSV format')
     parser.add_argument(
         'inactivity_period_file',
@@ -13,11 +13,12 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     raw_data = get_raw_log_data(args.log)
-    print args.log
-    print args.output
-    print args.inactivity_period_file
-
     inactivity_value = get_inactivity_value(args.inactivity_period_file)
+
+    try:
+        remove(args.output)
+    except OSError:
+        pass
 
     with open(args.output, 'a') as output_file:
         process_logs(raw_data, inactivity_value, output_file)
