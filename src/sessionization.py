255de998e4118a1883cc884f56346486c5f21f83
Brandon Krull <btkrull@gmail.com>
2018-05-23 22:05:10 -0700

    fixing arg order to align with run.sh

--- src/sessionization.py
+++ src/sessionization.py
@@ -1,5 +1,3 @@
-#! /usr/bin/env python
-
 from argparse import ArgumentParser
 from processor import get_raw_log_data, process_logs
 
@@ -7,17 +5,23 @@ if __name__ == '__main__':
     parser = ArgumentParser()
 
     parser.add_argument('log', help='Name of input log file in CSV format')
-    parser.add_argument('output', help='Name of output log file in CSV format')
     parser.add_argument(
-        'inactivity_period',
+        'inactivity_period_file',
         help='Name of file that defines length of inactivity period')
+    parser.add_argument('output', help='Name of output log file in CSV format')
 
     args = parser.parse_args()
 
     raw_data = get_raw_log_data(args.log)
     print args.log
     print args.output
-    print args.inactivity_period
-    with open(args.output, 'w') as handle:
-        process_logs(raw_data, args.inactivity_period, handle)
+    print args.inactivity_period_file
 
+    with open(args.inactivity_period_file) as f:
+        try:
+            inactivity_period = int(f.readline())
+        except ValueError:
+            raise ValueError('invalid inactivity value')
+
+    with open(args.output, 'a') as output_file:
+        process_logs(raw_data, inactivity_period, output_file)
